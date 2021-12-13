6ac9059dcadc47f8c73759f3fb79a6ec5b6430f4
Aernir Ridley <ajepson5229@gmail.com>
2021-12-12 22:48:20 -0500

    Finished verification and added infrastucture for Heroku Deployment.

--- config/config.php
+++ config/config.php
@@ -5,32 +5,44 @@
     $configData = parse_ini_file(__DIR__ . "/config.ini");
     
     //EVE AUTHENTICATION CONFIGURATION
-    $configVariables["Client ID"] = $configData["ClientID"];
-    $configVariables["Client Secret"] = $configData["ClientSecret"];
-    $configVariables["Client Scopes"] = $configData["ClientScopes"];
-    $configVariables["Default Scopes"] = $configData["DefaultScopes"];
-    $configVariables["Client Redirect"] = $configData["ClientRedirect"];
-    $configVariables["Auth Type"] = $configData["AuthType"];
-    $configVariables["Super Admins"] = explode(",", str_replace(" ", "", $configData["SuperAdmins"]));
+    $configVariables["Client ID"] = (isset($_ENV["Client_ID"])) ? $_ENV["Client_ID"] : $configData["ClientID"];
+    $configVariables["Client Secret"] = (isset($_ENV["Client_Secret"])) ? $_ENV["Client_Secret"] : $configData["ClientSecret"];
+    $configVariables["Client Scopes"] = (isset($_ENV["Client_Scopes"])) ? $_ENV["Client_Scopes"] : $configData["ClientScopes"];
+    $configVariables["Default Scopes"] = (isset($_ENV["Default_Scopes"])) ? $_ENV["Default_Scopes"] : $configData["DefaultScopes"];
+    $configVariables["Client Redirect"] = (isset($_ENV["Client_Redirect"])) ? $_ENV["Client_Redirect"] : $configData["ClientRedirect"];
+    $configVariables["Auth Type"] = (isset($_ENV["Auth_Type"])) ? $_ENV["Auth_Type"] : $configData["AuthType"];
+    $configVariables["Super Admins"] = (isset($_ENV["Super_Admins"])) ? explode(",", str_replace(" ", "", $_ENV["Super_Admins"])) : explode(",", str_replace(" ", "", $configData["SuperAdmins"]));
     
     //NEUCORE AUTHENTICATION CONFIGURATION
-    $configVariables["NeuCore ID"] = $configData["AppID"];
-    $configVariables["NeuCore Secret"] = $configData["AppSecret"];
-    $configVariables["NeuCore URL"] = $configData["AppURL"];
-
+    $configVariables["NeuCore ID"] = (isset($_ENV["App_ID"])) ? $_ENV["App_ID"] : $configData["AppID"];
+    $configVariables["NeuCore Secret"] = (isset($_ENV["App_Secret"])) ? $_ENV["App_Secret"] : $configData["AppSecret"];
+    $configVariables["NeuCore URL"] = (isset($_ENV["App_URL"])) ? $_ENV["App_URL"] : $configData["AppURL"];
+    
     //DATABASE SERVER CONFIGURATION
-    $configVariables["Database Server"] = $configData["DatabaseServer"] . ":" . $configData["DatabasePort"];
-    $configVariables["Database Username"] = $configData["DatabaseUsername"];
-    $configVariables["Database Password"] = $configData["DatabasePassword"];
+    $configVariables["Database Server"] = (isset($_ENV["Database_Server"])) ? $_ENV["Database_Server"] : ($configData["DatabaseServer"] . ":" . $configData["DatabasePort"]);
+    $configVariables["Database Username"] = (isset($_ENV["Database_Username"])) ? $_ENV["Database_Username"] : $configData["DatabaseUsername"];
+    $configVariables["Database Password"] = (isset($_ENV["Database_Password"])) ? $_ENV["Database_Password"] : $configData["DatabasePassword"];
     
     //DATABASE NAME CONFIGURATION
-    $configVariables["Database Name"] = $configData["DatabaseName"];
+    $configVariables["Database Name"] = (isset($_ENV["Database_Name"])) ? $_ENV["Database_Name"] : $configData["DatabaseName"];
+    
+    //JawsDB Maria Support (For Heroku)
+    if (isset($_ENV["JAWSDB_MARIA_URL"])) {
+        
+        $jawsDBURL = parse_url($_ENV["JAWSDB_MARIA_URL"]);
+        
+        $configVariables["Database Server"] = $jawsDBURL["host"];
+        $configVariables["Database Username"] = $jawsDBURL["user"];
+        $configVariables["Database Password"] = $jawsDBURL["pass"];
+        $configVariables["Database Name"] = ltrim($jawsDBURL["path"], "/");
+        
+    }
     
     //SITE CONFIGURATION
-    $configVariables["Auth Cookie Name"] = $configData["AuthCookieName"];
-    $configVariables["Session Time"] = $configData["SessionTime"];
-    $configVariables["Auth Cache Time"] = $configData["AuthCacheTime"];
-    $configVariables["Max Table Rows"] = $configData["MaxTableRows"];
-    $configVariables["Store Visitor IPs"] = boolval($configData["StoreVisitorIPs"]);
+    $configVariables["Auth Cookie Name"] = (isset($_ENV["Auth_Cookie_Name"])) ? $_ENV["Auth_Cookie_Name"] : $configData["AuthCookieName"];
+    $configVariables["Session Time"] = (isset($_ENV["Session_Time"])) ? $_ENV["Session_Time"] : $configData["SessionTime"];
+    $configVariables["Auth Cache Time"] = (isset($_ENV["Auth_Cache_Time"])) ? $_ENV["Auth_Cache_Time"] : $configData["AuthCacheTime"];
+    $configVariables["Max Table Rows"] = (isset($_ENV["Max_Table_Rows"])) ? $_ENV["Max_Table_Rows"] : $configData["MaxTableRows"];
+    $configVariables["Store Visitor IPs"] = (isset($_ENV["Store_Visitor_IPs"])) ? boolval($_ENV["Store_Visitor_IPs"]) : boolval($configData["StoreVisitorIPs"]);
     
 ?>
--- src/apis/Verify/Api.php
+++ src/apis/Verify/Api.php
@@ -7,6 +7,7 @@
         private $databaseConnection;
         private $logger;
         private $configVariables;
+        private $urlData;
         
         public function __construct(
             private \Ridley\Core\Dependencies\DependencyManager $dependencies
@@ -15,6 +16,89 @@
             $this->databaseConnection = $this->dependencies->get("Database");
             $this->logger = $this->dependencies->get("Logging");
             $this->configVariables = $this->dependencies->get("Configuration Variables");
+            $this->urlData = $this->dependencies->get("URL Data");
+            
+            if ($this->urlData["Page Topic"] !== false and $this->urlData["Page Number"] !== false) {
+                
+                $this->verifyResponse($this->urlData["Page Topic"], $this->urlData["Page Number"]);
+                
+            }
+            elseif ($this->urlData["Page Topic"] !== false) {
+                
+                $this->getAllResponses($this->urlData["Page Topic"]);
+                
+            }
+            else {
+                
+                header($_SERVER["SERVER_PROTOCOL"] . " 400 Bad Request");
+                trigger_error("Invalid URL Format for a Verification Request.", E_USER_ERROR);
+                
+            }
+            
+        }
+        
+        private function getAllResponses($verificationToken) {
+            
+            $responseQuery = $this->databaseConnection->prepare("SELECT usertokens.verificationtoken, usertokens.charactername, usertokens.corename FROM usertokens INNER JOIN polls ON usertokens.pollid = polls.id WHERE polls.verificationtoken = :verificationtoken;");
+            $responseQuery->bindParam(":verificationtoken", $verificationToken);
+            $responseQuery->execute();
+            $responseData = $responseQuery->fetchAll();
+            
+            if (!empty($responseData)) {
+                
+                $output = new \SimpleXMLElement("<xml/>");
+                
+                foreach ($responseData as $eachResponse) {
+                    
+                    $newResponder = $output->addChild("responder");
+                    $newResponder->addChild("verification-token", $eachResponse["verificationtoken"]);
+                    $newResponder->addChild("character-name", $eachResponse["charactername"]);
+                    $newResponder->addChild("core-name", $eachResponse["corename"]);
+                    
+                }
+                
+                Header("Content-Type: text/xml");
+                echo $output->asXML();
+                
+            }
+            else {
+                
+                header($_SERVER["SERVER_PROTOCOL"] . " 400 Bad Request");
+                
+            }
+            
+        }
+        
+        private function verifyResponse($verificationToken, $userToken) {
+            
+            $responseQuery = $this->databaseConnection->prepare("SELECT usertokens.verificationtoken, usertokens.charactername, usertokens.corename FROM usertokens INNER JOIN polls ON usertokens.pollid = polls.id WHERE polls.verificationtoken = :verificationtoken AND usertokens.verificationtoken = :usertoken;");
+            $responseQuery->bindParam(":verificationtoken", $verificationToken);
+            $responseQuery->bindParam(":usertoken", $userToken);
+            $responseQuery->execute();
+            $responseData = $responseQuery->fetchAll();
+            
+            if (!empty($responseData)) {
+                
+                $output = new \SimpleXMLElement("<xml/>");
+                
+                foreach ($responseData as $eachResponse) {
+                    
+                    $newResponder = $output->addChild("responder");
+                    $newResponder->addChild("verification-token", $eachResponse["verificationtoken"]);
+                    $newResponder->addChild("character-name", $eachResponse["charactername"]);
+                    $newResponder->addChild("core-name", $eachResponse["corename"]);
+                    
+                }
+                
+                Header("Content-Type: text/xml");
+                echo $output->asXML();
+                
+            }
+            else {
+                
+                header($_SERVER["SERVER_PROTOCOL"] . " 400 Bad Request");
+                
+            }
             
         }
         
